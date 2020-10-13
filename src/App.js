23f614f905e3470016516e1ded0d51c9df452f22
Michaeldremy <michaeldremy@gmail.com>
2020-10-13 10:46:13 -0700

    Integrated Alan AI for voice calls and setup first command which is to call Alan AI and get the news from X source

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
+const alanKey = '685e6de5dc635cc19ce9af14aa4a6a022e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 function App() {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      // creating alan commands
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+        }
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Alan AI News Application</h1>
     </div>
   );
 }
