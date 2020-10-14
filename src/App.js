a83f06366fc6126defd135c479a5a3b1c64793e9
Michaeldremy <michaeldremy@gmail.com>
2020-10-13 19:33:11 -0700

    Added new command that allows Alan to OPEN a new article and direct to the articles URL. Also added a 'go back' command that will bring back to the hompage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import alanBtn from '@alan-ai/alan-sdk-web';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import "./App.css";
 
-import NewsCards from './components/NewsCards/NewsCards';
-import useStyles from './styles';
+// This will parse our numbers
+import wordsToNumbers from "words-to-numbers";
 
-const alanKey = '685e6de5dc635cc19ce9af14aa4a6a022e956eca572e1d8b807a3e2338fdd0dc/stage';
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
+
+const alanKey =
+  "685e6de5dc635cc19ce9af14aa4a6a022e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
   const [newsArticles, setNewsArticles] = useState([]);
@@ -16,23 +20,39 @@ function App() {
     alanBtn({
       key: alanKey,
       // creating alan commands
-      onCommand: ({ command, articles }) => {
-        if (command === 'newHeadlines') {
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiceArticle(-1);
-        } else if (command === 'highlight') {
+        } else if (command === "highlight") {
           setActiceArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again.");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening Article");
+          }
         }
-      }
-    })
-  }, [])
+      },
+    });
+  }, []);
 
   return (
     <div>
       <div className={classes.logoContainer}>
-        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="alan logo"
+        />
       </div>
-      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
